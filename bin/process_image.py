ba1a5fb7c46475459b3e650cee2d6f69da427742
Ranbir Aulakh <RanbirAulakh@users.noreply.github.com>
2023-10-17 10:06:22 -0400

    feat: Update github actions & lint (#9)

--- bin/process_image.py
+++ bin/process_image.py
@@ -64,7 +64,7 @@ deployed_images: dict = {
     "tile_png": f"s3://{image_bucket}/tile.png",
     "sicd_capella_chip_ntf": f"s3://{image_bucket}/sicd-capella-chip.ntf",
     "sicd_umbra_chip_ntf": f"s3://{image_bucket}/sicd-umbra-chip.ntf",
-    "sicd_interferometric_hh_ntf": f"s3://{image_bucket}/sicd-interferometric-hh.nitf"
+    "sicd_interferometric_hh_ntf": f"s3://{image_bucket}/sicd-interferometric-hh.nitf",
 }
 
 # call into root directory of this package so that we can run this script from anywhere.
