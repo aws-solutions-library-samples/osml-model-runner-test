42ac9dda107d17b13b1eac96f888556860315223
drduhe <drduhe@amazon.com>
2023-10-26 13:06:22 -0600

    Refactoring load tests

--- src/aws/osml/utils/integ_utils.py
+++ src/aws/osml/utils/integ_utils.py
@@ -21,11 +21,11 @@ def run_model_on_image(
     """
     The workflow to build an image request for a specific SM endpoint and then place it
     on the corresponding SQS queue for ModelRunner to pick up and process. Once the image
-    has been completed return the associated image_id and image_request object for analysis.
+    has been completed, return the associated image_id and image_request object for analysis.
 
-    :param sqs_client: boto3.resource = the sqs client fixture passed in
-    :param sm_endpoint: str = the SM endpoint you wish to run your image against
-    :param kinesis_client: Optional[boto3.resource] = the optional kinesis client fixture passed in
+    :param sqs_client: Sqs client fixture passed in
+    :param sm_endpoint: SM endpoint you wish to run your image against
+    :param kinesis_client: Optional kinesis client fixture passed in
 
     :return: Tuple[str, str, Dict[str, Any], Dict[str, Any]] = the generated image_id, job_id, image_request,
              and kinesis shard.
@@ -61,8 +61,8 @@ def queue_image_processing_job(sqs_client: boto3.resource, image_processing_requ
     Place an image_request object onto the associated SQS queue for ModelRunner
     pick up for processing.
 
-    :param sqs_client: boto3.resource = the sqs client fixture passed in
-    :param image_processing_request: Dict[str, Any] = the image request to place in the queue.
+    :param sqs_client: Sqs client fixture passed in
+    :param image_processing_request: Image request to place in the queue.
 
     :return: None
     """
@@ -81,13 +81,11 @@ def queue_image_processing_job(sqs_client: boto3.resource, image_processing_requ
         logging.error(f"Unable to send job request to SQS queue: {OSMLConfig.SQS_IMAGE_REQUEST_QUEUE}")
         logging.error(f"{error}")
         assert False
-        return None
 
     except ParamValidationError as error:
         logging.error("Invalid SQS API request; validation failed")
         logging.error(f"{error}")
         assert False
-        return None
 
 
 def monitor_job_status(sqs_client: boto3.resource, image_id: str) -> None:
@@ -95,8 +93,8 @@ def monitor_job_status(sqs_client: boto3.resource, image_id: str) -> None:
     Monitors the status of the image request on the corresponding SQS queue and returns
     once the image request associated with the given image_id has completed.
 
-    :param sqs_client: boto3.resource = the sqs client fixture passed in
-    :param image_id: str = the image_id associated with the image request to monitor
+    :param sqs_client: The sqs client fixture passed in
+    :param image_id: Image_id associated with the image request to monitor
     :return: None
     """
     done = False
@@ -149,7 +147,6 @@ def get_kinesis_shard(kinesis_client: boto3.client) -> Dict[str, Any]:
     """
     Get uniquely identified sequence of data records in a kinesis stream
 
-    :param date_time: datetime.datetime: the time at which the test started running and read results starting from
     :param kinesis_client: boto3.client = the kinesis client fixture passed in
 
     :return: Dict[str, Any] = uniquely identified sequence of data records
@@ -171,7 +168,7 @@ def validate_features_match(
     kinesis_client: boto3.client = None,
 ) -> None:
     """
-    Compares known standard results ( features ) against the ones generated from the tests.
+    Compares known standard results (features) against the ones generated from the tests.
 
     :param image_processing_request: Dict[str, Any] = the image processing request to validate against
     :param job_id: str = the job-id associated with the request
@@ -191,7 +188,7 @@ def validate_features_match(
         expected_features = geojson.load(geojson_file)["features"]
 
     while not done and max_retries > 0:
-        # This is really a List[Sink] but we don't have a common library to pull
+        # This is really a List[Sink], but we don't have a common library to pull
         # this from so using "any"
         outputs: List[Dict[str, Any]] = image_processing_request["outputs"]
         # Reset found outputs on each retry so that we don't count a single output
@@ -218,12 +215,12 @@ def validate_s3_features_match(bucket: str, prefix: str, expected_features: List
     """
     Checks a s3 output sync against known good feature results for a given test image.
 
-    :param bucket: str = a folder inside s3
-    :param prefix: str = string of characters at the beginning of the object key name
-    :param expected_features: List[Features] = the list of features
-    :param s3_client: boto3.client = the s3 client fixture passed in
+    :param bucket: Folder inside s3
+    :param prefix: String of characters at the beginning of the object key name
+    :param expected_features: The list of features
+    :param s3_client: S3 client fixture passed in
 
-    :return: bool = s3 features matches the expected features
+    :return: True if S3 features match the expected features
     """
     logging.info(f"Checking S3 at '{bucket}/{prefix}' for results.")
     for object_key in get_matching_s3_keys(
@@ -255,13 +252,13 @@ def validate_kinesis_features_match(
     """
     Checks a Kinesis output sync against know good feature results for a given test image
 
-    :param job_id: str = the job-id associated with the request
-    :param stream: str = kinesis stream name
-    :param shard_iter: Dict[str, Any] = uniquely identified sequence of data records
-    :param expected_features: List[Feature] = the list of features
-    :param kinesis_client: boto3.client = the kinesis client fixture passed in
+    :param job_id: Job-id associated with the request
+    :param stream: Kinesis stream name
+    :param shard_iter: Uniquely identified record sequence
+    :param expected_features: List of features expected by test
+    :param kinesis_client: Kinesis client fixture passed in
 
-    :return: bool = kinesis features matches the expected features
+    :return: Kinesis features matches the expected features
     """
     logging.info(f"Checking Kinesis Stream '{stream}' for results.")
     records = kinesis_client.get_records(ShardIterator=shard_iter, Limit=10000)["Records"]
@@ -286,10 +283,10 @@ def get_matching_s3_keys(s3_client: boto3.client, bucket: str, prefix: str = "",
     """
     Generate the keys in an S3 bucket.
 
-    :param s3_client: boto3.client = Boto3 S3 Client.
-    :param bucket: str = Name of the S3 bucket.
-    :param prefix: str = Only fetch keys that start with this prefix (optional).
-    :param suffix: str = Only fetch keys that end with this suffix (optional).
+    :param s3_client: Boto3 S3 Client.
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Only fetches keys that start with this prefix (optional).
+    :param suffix: Only fetches keys that end with this suffix (optional).
 
     :return: None
     """
@@ -310,14 +307,14 @@ def get_matching_s3_keys(s3_client: boto3.client, bucket: str, prefix: str = "",
 def feature_equal(expected: geojson.Feature, actual: geojson.Feature) -> bool:
     """
     Determines if two features are roughly equivalent. We can't compare the full object because
-    each feature include a hex token as a top level property. Instead, we compare every other part of
+    each feature includes a hex token as a top level property. Instead, we compare every other part of
     the feature to make sure they match. The expected data located in ./src/data has a placeholder
     value, $IMAGE_ID$, in place of an actual image id so that we can swap it in.
 
-    :param expected: geojson.Feature = the feature we expect to match the result
-    :param actual: geojson.Feature = the feature that was generated by the test
+    :param expected: Feature we expect to match the result
+    :param actual: Feature that was generated by the test
 
-    :return: bool = whether the features match
+    :return: Whether the features match
     """
     actual_pixel_coords = actual.get("properties", {}).get("detection", {}).get("pixelCoordinates")
     expected_pixel_coords = expected.get("properties", {}).get("detection", {}).get("pixelCoordinates")
@@ -337,16 +334,16 @@ def feature_collections_equal(expected: List[geojson.Feature], actual: List[geoj
     """
     Determines whether the supplied unordered feature lists are equal
 
-    :param expected: List[geojson.Feature] = unordered list of expected features
-    :param actual: List[geojson.Feature] = unordered list of detected features
+    :param expected: An unordered list of expected features
+    :param actual: An unordered list of detected features
 
-    :return: bool = check to see if expected and actual are equal
+    :return: Check to see if expected and actual are equal
     """
     if not len(expected) == len(actual):
         logging.info(f"Expected {len(expected)} features but found {len(actual)}")
         return False
     # The order of features will depend on the order that regions/tiles were fed to the model.
-    # While the order should roughly be the same there isn't a guarantee, we're just checking
+    # While the order should roughly be the same, there isn't a guarantee, we're just checking
     # that all expected features were detected here as order within the collection isn't
     # actually important.
     expected.sort(key=lambda x: str(x.get("properties", {}).get("detection", {}).get("pixelCoordinates")))
@@ -363,14 +360,14 @@ def feature_collections_equal(expected: List[geojson.Feature], actual: List[geoj
 def build_image_processing_request(sm_endpoint: str, image_url: str) -> Dict[str, Any]:
     """
     Build an image_processing_request meant to be placed on the corresponding ModelRunner SQS queue.
-    The image request is configured from the hydra provisioned hydra environment. In the future
-    this could, and probably should, be extended to build more variant image requests for additional
+    The image request is configured from test environment.
+    In the future this could, and probably should, be extended to build more variant image requests for additional
     testing configurations.
 
-    :param image_url: the URL to the image you want to process
-    :param sm_endpoint: str = the SM model endpoint that you want to build the image_request for
+    :param image_url: URL to the image you want to process
+    :param sm_endpoint: SM model endpoint that you want to build the image_request for
 
-    :return: Dict[str, Any] = the dictionary representation of the image request
+    :return: Dictionary representation of the image request
     """
     if OSMLConfig.KINESIS_RESULTS_STREAM:
         result_stream = OSMLConfig.KINESIS_RESULTS_STREAM
@@ -415,10 +412,10 @@ def count_features(image_id: str, ddb_client: boto3.resource) -> int:
     Counts the features present in the given DDB table associated
     with an image_id (hash_key)
 
-    :param image_id: str = the image_id the features are associated with
-    :param ddb_client: boto3.resource = the DDB client fixture
+    :param image_id: Image_id features are associated with
+    :param ddb_client: DDB client fixture
 
-    :return: int = count of the features found in the table for the image id
+    :return: Count of the features found in the table for the image id
     """
     ddb_table = ddb_client.Table(OSMLConfig.DDB_FEATURES_TABLE)
 
@@ -442,7 +439,7 @@ def validate_expected_feature_count(feature_count: int) -> None:
     """
     Validate the number of features created match expected values
 
-    :param feature_count: int = the numer of features found for an image
+    :param feature_count: Number of features found for an image
 
     :return: None
     """
@@ -460,10 +457,11 @@ def validate_expected_feature_count(feature_count: int) -> None:
 def get_expected_image_feature_count(image: str) -> int:
     """
     Get the number of expected features that are created for each image,
-    since these are random we want to just count the features in the table
+    since these are random.
+    We want to just count the features in the table,
     not match them against any known results.
 
-    :return: int = features we expect for a given image using the flood model
+    :return: Features we expect for a given image using the flood model
     """
     # Set the expected
     if "large" in image:
@@ -482,9 +480,9 @@ def query_items(ddb_table: boto3.resource, hash_key: str, hash_value: str) -> Li
     """
     Query the table for all items of a given hash_key and hash_value.
 
-    :param ddb_table: boto3.resource = the DynamoDB table you wish to query
-    :param hash_key: str = the hash key of the items you wish to scan
-    :param hash_value: str = the hash key value of the items you wish to scan
+    :param ddb_table: DynamoDB table you wish to query
+    :param hash_key: The Hash key associated with the items you wish to scan
+    :param hash_value: The Hash key value of the items you wish to scan
 
     :return: List[Dict[str, Any]] = the list of dictionary responses corresponding to the items returned
     """
@@ -516,10 +514,10 @@ def count_region_request_items(image_id: str, ddb_client: boto3.resource) -> int
     Counts the region request present in the given DDB table associated
     with an image_id (hash_key)
 
-    :param image_id: str the image_id the region request are associated with
-    :param ddb_client: boto3.resource = the DDB client fixture
+    :param image_id: Image_id region request is associated with
+    :param ddb_client: DDB client fixture
 
-    :return: int = count of the region request found in the table for the image id
+    :return: Count of the region request found in the table for the image id
     """
     ddb_region_request_table = ddb_client.Table(OSMLConfig.DDB_REGION_REQUEST_TABLE)
     items = query_items(ddb_region_request_table, "image_id", image_id)
@@ -535,9 +533,9 @@ def count_region_request_items(image_id: str, ddb_client: boto3.resource) -> int
 
 def validate_expected_region_request_items(region_request_count: int) -> None:
     """
-    Validate the number of region request created match expected values
+    Validate the number of region requests created match-expected values
 
-    :param region_request_count: int = the numer of region request found for an image
+    :param region_request_count: Number of region requests found for an image
 
     :return: None
     """
@@ -554,9 +552,9 @@ def validate_expected_region_request_items(region_request_count: int) -> None:
 
 def get_expected_region_request_count(image: str) -> int:
     """
-    Get the expected number of region request were created for each image
+    Get the expected number of region requests was created for each image
 
-    :return: int = region request we expect for a given image using the flood model
+    :return: Region request we expect for a given image using the flood model
     """
     expected_count = 0
     if "small" in image:
